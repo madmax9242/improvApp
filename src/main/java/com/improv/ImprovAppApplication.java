74bd17c9a7ae7cc1f7ae478a68730e63bb93e02f
DESKTOP-M359TO2 <maxd924@gmail.com>
2020-08-03 13:49:18 -0400

    added spring boot

---
+++ src/main/java/com/improv/ImprovAppApplication.java
@@ -0,0 +1,13 @@
+package com.improv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImprovAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImprovAppApplication.class, args);
+	}
+
+}
